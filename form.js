2bd61ee65b871bbb25c7718da9d9cc5b3481d3e4
PriyaSingarapu <priyanka@schemaxtech.com>
2021-08-04 23:01:08 +0530

    validationform

---
+++ form.js
@@ -0,0 +1,66 @@
+
+function validation()
+
+{
+
+var firstname = document.getElementById("fname");
+    if (firstname.value == "") {
+        alert("Please enter your firstname");
+        return false;
+    }
+    else
+    {
+        true
+    }
+
+    var lastname = document.getElementById("lname");
+    if (lastname.value == "") {
+        alert("Please enter your lastname");
+        return false;
+    }
+    else
+    {
+        true
+    }
+
+    var fathername = document.getElementById("fathername");
+    if (fathername.value== "") {
+        alert("please enter your fathername");
+        return false;
+    }
+    else
+    {
+        true
+    }
+
+    var mothername = document.getElementById("mothername");
+    if (mothername.value == "") {
+        alert("Please enter your mothername");
+        return false;
+    }
+    else
+    {
+        true
+    }
+
+    var adharnumber = document.getElementById("adharnumber");
+    if (adharnumber.value == "") {
+        alert("Please enter your adharnumber");
+        return false;
+    }
+    else
+    {
+        true
+    }
+
+    var phonenumber = document.getElementById("phonenumber");
+    if (phonenumber.value == "") {
+        alert("Please enter your phonenumber");
+        return false;
+    }
+    else
+    {
+        true
+    }
+}
+
